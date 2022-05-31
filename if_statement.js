c340b4466323e441b88e0e28c3107d654dc897a6
Kazungu1 <kazunguderriq@gmail.com>
2022-05-31 11:31:12 +0300

    add ternary operator

--- if_statement.js
+++ if_statement.js
@@ -12,6 +12,13 @@ let age =2022-myBirthyear;
 let nationalId;
 if(age>=18){
     nationalId = true;
-    
+
 }
 console.log(nationalId)
+
+// else using ternary operator - returns a falsey value
+const applicantAge =17;
+let applicationStatus;
+applicantAge >=18 ? (applicationStatus ='Approve loan'):(applicationStatus='Loan Denied');
+console.log(applicationStatus)
+
