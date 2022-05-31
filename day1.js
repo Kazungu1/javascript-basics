9ba6d17900cad10225c58fd8bb025341c978a0a0
Kazungu1 <kazunguderriq@gmail.com>
2022-05-31 11:00:31 +0300

    add selection statement

--- day1.js
+++ day1.js
@@ -1,5 +1,12 @@
 // Statements in Javascript
 // there are 2 main type of Statements: selection and repetition
 // selection statements: execute some parts of the codeblock based on a certain condition;
-// if statement
+// the following are basics of repetition and selection statements.
 
+// if statement-selection statement.
+// the code below checks if the value is greater than 32, if so it increments the value by 10.
+let favouriteNUmber =02;
+if(favouriteNUmber>=10){
+    favouriteNUmber +=10;
+}
+console.log(favouriteNUmber);
