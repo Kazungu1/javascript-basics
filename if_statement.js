11087a4c26e49ca02511303d725c374da2c5741a
Kazungu1 <kazunguderriq@gmail.com>
2022-05-31 11:18:44 +0300

    if statement example

---
+++ if_statement.js
@@ -0,0 +1,17 @@
+//  if statement
+const myScore= 80;
+let levelComment;
+if(myScore>70){
+    levelComment ="Congratulations";
+};
+console.log(levelComment)
+
+// example 2
+const myBirthyear = 1998;
+let age =2022-myBirthyear;
+let nationalId;
+if(age>=18){
+    nationalId = true;
+    
+}
+console.log(nationalId)
