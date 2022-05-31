b99f1dbf9ee0a3e510be02d72d4463b27a294f8e
Kazungu1 <kazunguderriq@gmail.com>
2022-05-31 20:22:16 +0300

    add else if statements

--- switch_statement.js
+++ switch_statement.js
@@ -1,1 +1,14 @@
-// switch statement
+// if else statements
+const name = "JoyLiz";
+let greeting;
+if(name === 'JoyLiz'){
+    greeting = 'Hello Joyliz'
+} else if(name === 'Riziki'){
+    greeting = 'Next time come on time'
+}
+else if(name === 'Andrew'){
+    greeting = 'Hello Medic'
+} else{
+    greeting = 'Good eveining Abdul'
+}
+console.log(greeting)
