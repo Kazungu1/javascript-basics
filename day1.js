d8480f04c9e093dd1230d481e547a5448f15b703
Kazungu1 <kazunguderriq@gmail.com>
2022-05-31 10:55:48 +0300

    day2 of javscript learning

---
+++ day1.js
@@ -0,0 +1,5 @@
+// Statements in Javascript
+// there are 2 main type of Statements: selection and repetition
+// selection statements: execute some parts of the codeblock based on a certain condition;
+// if statement
+
