60325c90b576179ecca89a08ab3de2e700a94f99
Kazungu1 <kazunguderriq@gmail.com>
2022-06-02 09:29:58 +0300

    add nested array as a matrix

--- array.js
+++ array.js
@@ -2,7 +2,7 @@
 // creating a new array
 const friendsAge = [23,22,24,21,19,20];
 
-// array of different data types
+// array of different data types and nested arrays
 const myArray = ['sam','grevians',true,23,56,[23,54,78]];
 console.log(myArray);
 
@@ -22,3 +22,13 @@ console.log(friendsAge[2]);
 // updating elements in arrays
 myArray[0] = 'Samuel';
 console.log(myArray[0]);
+
+// array as 2d matrix to replace nesting
+const myArrays = [
+    [1,2,3],
+    ['kim','sam','Ken'],
+    [true, false,true,false],
+];
+console.log(myArrays);
+// accessing element of nested arrays
+console.log(myArrays[0][0]);
